3abb08cda015a4619b19da98d2ebd434fcfdb57a
aiwizzard <ajmalekvsroad@gmail.com>
2023-07-04 22:31:18 +0530

    intial t3 ws working chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+// src/pages/_app.tsx
+import { withTRPC } from "@trpc/next";
+import type { AppRouter } from "../server/router";
+import type { AppType } from "next/dist/shared/lib/utils";
+import superjson from "superjson";
+
 import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import { createWSClient, wsLink } from "@trpc/client/links/wsLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
-
-const MyApp: AppType<{ session: Session | null }> = ({
+const MyApp: AppType = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
@@ -17,4 +20,63 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-export default api.withTRPC(MyApp);
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+const url = `${getBaseUrl()}/api/trpc`;
+
+function getEndingLink() {
+  if (typeof window === "undefined") {
+    return httpBatchLink({
+      url,
+    });
+  }
+
+  const client = createWSClient({
+    url: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:3001",
+  });
+
+  return wsLink<AppRouter>({
+    client,
+  });
+}
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+
+    return {
+      links: [
+        // pass logger link in here
+        getEndingLink(),
+      ],
+      transformer: superjson,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      headers: () => {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+          };
+        }
+        return {};
+      },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: false,
+})(MyApp);
